14739c8d779af408d278049bcabd4f5ed140c087
Rajshekar-21 <156403241+Rajshekar-21@users.noreply.github.com>
2025-03-11 15:37:40 +0530

    Update praveen.java

--- praveen.java
+++ praveen.java
@@ -1,7 +1,8 @@
 public class praveen{
-public static void main(String args[]){
-int a=5;
-int b=7;
-int sum=a+b;
-System.out.println("sum is:"+sum);}
+  public static void main(String args[]){
+    int a=5;
+    int b=7;
+    int sum=a+b;
+  System.out.println("sum is:"+sum);
+  }
 }
